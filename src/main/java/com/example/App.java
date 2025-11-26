e003fa5d9241eb73096f5bf9708d380f3f165664
jimmyjcp <jimmyjcp@gmail.com>
2025-11-26 14:57:10 -0500

    Add App class with main and sumar methods

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hola desde Java en GitHub Actions!");
+    }
+
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+}
